04f78546497102e0c43553f531a00d8def8f4d7b
unknown <pulkit.sharma@roadcast.net>
2025-03-27 12:18:34 +0530

    fixes UI

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -196,6 +196,40 @@ $(document).ready(function(){
 
 
 
+    // accordion | Start
+    $(document).ready(function () {
+        $(".accordion-button").click(function () {
+            let content = $(this).next(".accordion-content");
+            let icon = $(this).find(".icon");
+
+            // Close all other accordions
+            $(".accordion-content").not(content).slideUp().removeClass("open");
+            $(".icon").html(`
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                    <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+                </svg>
+            `);
+
+            // Toggle clicked accordion
+            if (content.hasClass("open")) {
+                content.slideUp().removeClass("open");
+                icon.html(`
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                        <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+                    </svg>
+                `);
+            } else {
+                content.slideDown().addClass("open");
+                icon.html(`
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4">
+                        <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+                    </svg>
+                `);
+            }
+        });
+    });
+
+
     
     
 
